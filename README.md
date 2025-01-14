b9ecfbcaedb0cbaba5d785d91806587ec3e43a30
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2025-01-14 21:06:57 +0000

    Add photo credits

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Trying out deploying to Github Pages using the Mantine Vite template
 
+## Photo credits:
 
-[Mantine Vite Documentation](https://mantine.dev/guides/vite/)
+### Taronga Zoo (Sydney)
+https://taronga.org.au/media-release/2019-04-01/three-rare-tiger-cubs-debut-taronga-zoo-sydney
+
+### Pngtree
+https://pngtree.com/freebackground/baby-tiger-cub-is-walking-on-rocks_2663035.html
+
+### Nashville Zoo
+https://fox17.com/news/local/gallery/nashville-zoo-celebrates-birth-of-three-sumatran-tiger-cubs-marking-joyful-milestone?photo=14
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import "@mantine/core/styles.css";
 import { Carousel } from '@mantine/carousel';
-import { MantineProvider, Group, Image } from "@mantine/core";
+import { MantineProvider, Group, Image, Tooltip } from "@mantine/core";
 import { theme } from "./theme";
 
 
 function Demo() {
   return (
     <Group>
-      Hey Hey!
+      Roar!
       <Carousel withIndicators height={500}>
-      <Carousel.Slide><Image  h={500} w="auto" src="/stunning-waddle/tigerCubs.jpg"/></Carousel.Slide>
-      <Carousel.Slide><Image  h={500} w="auto" src="/stunning-waddle/babyTiger.jpg"/></Carousel.Slide>
-      <Carousel.Slide><Image   h={500} w="auto" src="/stunning-waddle/basketOfCubs.jpg"/></Carousel.Slide>
+      <Carousel.Slide>    
+        <Tooltip label="https://taronga.org.au/media-release/2019-04-01/three-rare-tiger-cubs-debut-taronga-zoo-sydney">
+          <Image h={500} w="auto" src="/stunning-waddle/tigerCubs.jpg"/>
+        </Tooltip>
+      </Carousel.Slide>
+      <Carousel.Slide>    
+        <Tooltip label="https://pngtree.com/freebackground/baby-tiger-cub-is-walking-on-rocks_2663035.html">
+          <Image h={500} w="auto" src="/stunning-waddle/babyTiger.jpg"/>
+        </Tooltip>
+      </Carousel.Slide>
+      <Carousel.Slide>    
+        <Tooltip label="https://fox17.com/news/local/gallery/nashville-zoo-celebrates-birth-of-three-sumatran-tiger-cubs-marking-joyful-milestone?photo=14">
+          <Image h={500} w="auto" src="/stunning-waddle/basketOfCubs.jpg"/>
+        </Tooltip>
+      </Carousel.Slide>
       </Carousel>
     </Group>
   );
