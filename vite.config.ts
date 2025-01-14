1fc57e81252ebef4d71b4222820be3415eaba55e
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2025-01-14 18:48:56 +0000

    Restore base to /stunning-waddle

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/stunning-waddle/",
+  base: "/stunning-waddle/",
   plugins: [react()],
   build: {
     copyPublicDir: false,
