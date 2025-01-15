b3580b59c7eed868cd211028d3b0646a9ef833ee
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2025-01-15 15:17:01 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import "@mantine/core/styles.css";
 import { Carousel } from '@mantine/carousel';
-import { MantineProvider, Group, Image, Tooltip } from "@mantine/core";
+import { MantineProvider, Group, Image, HoverCard, Anchor } from "@mantine/core";
 import { theme } from "./theme";
+
+
+interface ImageWithCreditProps{
+    URL: string;
+    creditURL: string;
+    creditStr: string;
+}
+
+function ImageWithCredit(props: ImageWithCreditProps) {
+  return (
+  <HoverCard shadow="md" openDelay={2000}>
+    <HoverCard.Target>
+      <Image h={500} w="auto" src={props.URL} radius="lg" />
+    </HoverCard.Target>
+    <HoverCard.Dropdown>
+      <Anchor href={props.creditURL} size="sm" c="violet">
+        {props.creditStr}
+      </Anchor>
+    </HoverCard.Dropdown>
+  </HoverCard>
+  );
+}
 
 
 function Demo() {
@@ -10,19 +32,25 @@ function Demo() {
       Roar!
       <Carousel withIndicators height={500}>
       <Carousel.Slide>    
-        <Tooltip label="https://taronga.org.au/media-release/2019-04-01/three-rare-tiger-cubs-debut-taronga-zoo-sydney">
-          <Image h={500} w="auto" src="/stunning-waddle/tigerCubs.jpg"/>
-        </Tooltip>
+        <ImageWithCredit 
+          URL ="/stunning-waddle/tigerCubs.jpg" 
+          creditURL="https://taronga.org.au/media-release/2019-04-01/three-rare-tiger-cubs-debut-taronga-zoo-sydney"
+          creditStr="Taronga Zoo (Sydney)"
+          />
       </Carousel.Slide>
-      <Carousel.Slide>    
-        <Tooltip label="https://pngtree.com/freebackground/baby-tiger-cub-is-walking-on-rocks_2663035.html">
-          <Image h={500} w="auto" src="/stunning-waddle/babyTiger.jpg"/>
-        </Tooltip>
+      <Carousel.Slide> 
+        <ImageWithCredit 
+          URL ="/stunning-waddle/babyTiger.jpg"
+          creditURL="https://pngtree.com/freebackground/baby-tiger-cub-is-walking-on-rocks_2663035.html"
+          creditStr="Pngtree"
+        />
       </Carousel.Slide>
-      <Carousel.Slide>    
-        <Tooltip label="https://fox17.com/news/local/gallery/nashville-zoo-celebrates-birth-of-three-sumatran-tiger-cubs-marking-joyful-milestone?photo=14">
-          <Image h={500} w="auto" src="/stunning-waddle/basketOfCubs.jpg"/>
-        </Tooltip>
+      <Carousel.Slide>  
+        <ImageWithCredit 
+          URL ="/stunning-waddle/basketOfCubs.jpg"
+          creditURL="https://fox17.com/news/local/gallery/nashville-zoo-celebrates-birth-of-three-sumatran-tiger-cubs-marking-joyful-milestone?photo=14"
+          creditStr="Nashville Zoo"
+        />
       </Carousel.Slide>
       </Carousel>
     </Group>
